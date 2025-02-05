7450868f62bdaf5ef3843235e62e6cb27a50f73f
DixonzorCmpsi <dixonzor@gmail.com>
2025-02-05 18:10:48 -0500

    changed about and project

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,9 +9,9 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, My name is <span className="purple">Dixon Zor </span>
-            <span className="purple"> I am from the philadelphia area, </span>
+            <span className="purple"> and I am from the philadelphia area. </span>
             <br />
-            and is currently a student at Penn State, set to graduate in May of 2025.
+            I am currently a student at Penn State, set to graduate in May of 2025.
             <br />
             At the end of my studies I will earn a bachelor degree in computer science and technology.
             <br />
